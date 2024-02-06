414a783cd4a9aa77b12cfe15ee24a865a2dbf8a0
Collonville Thomas <collonville.thomas@gmail.com>
2024-02-07 00:28:12 +0100

    passage java 21

--- tc-osgi-bundle-neuronal-network-kohonen/src/main/java/org/tc/osgi/bundle/neuronalnetwork/kohonen/ihm/KohonenView.java
+++ tc-osgi-bundle-neuronal-network-kohonen/src/main/java/org/tc/osgi/bundle/neuronalnetwork/kohonen/ihm/KohonenView.java
@@ -7,32 +7,34 @@ import org.tc.osgi.bundle.neuronalnetwork.kohonen.layer.Kohonen;
 
 /**
  * KohonenView.java.
+ * 
  * @author Collonvillé Thomas
  * @version 0.0.1
  */
 @SuppressWarnings("serial")
 public class KohonenView extends View {
-    /**
-     * long serialVersionUID.
-     */
-    private static final long serialVersionUID = 8603581105287899638L;
+	/**
+	 * long serialVersionUID.
+	 */
+	private static final long serialVersionUID = 8603581105287899638L;
 
-    /**
-     * KohonenView constructor.
-     * @param model Kohonen
-     */
-    public KohonenView(final Kohonen model) {
-        super(model);
-    }
+	/**
+	 * KohonenView constructor.
+	 * 
+	 * @param model Kohonen
+	 */
+	public KohonenView(final Kohonen model) {
+		super(model);
+	}
 
-    /**
-     * @param g Graphics
-     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
-     */
-    @Override
-    public void paintComponent(final Graphics g) {
-        super.paintComponent(g);
-        final KohonenDraftman draftman = new KohonenDraftman(g);
-        ((Kohonen) getModel()).accept(draftman);
-    }
+	/**
+	 * @param g Graphics
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
+	@Override
+	public void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+		final KohonenDraftman draftman = new KohonenDraftman(g);
+		((Kohonen) getModel()).accept(draftman);
+	}
 }
--- tc-osgi-bundle-neuronal-network-kohonen/src/main/java/org/tc/osgi/bundle/neuronalnetwork/kohonen/layer/Kohonen.java
+++ tc-osgi-bundle-neuronal-network-kohonen/src/main/java/org/tc/osgi/bundle/neuronalnetwork/kohonen/layer/Kohonen.java
@@ -11,116 +11,122 @@ import org.tc.osgi.bundle.neuronalnetwork.metamodel.mm.Neurone;
 import org.tc.osgi.bundle.neuronalnetwork.metamodel.mm.Signal;
 import org.tc.osgi.bundle.neuronalnetwork.metamodel.mm.layers.ReseauNeuronal;
 import org.tc.osgi.bundle.neuronalnetwork.metamodel.mm.layers.UnidirectionnalLayer;
-import org.tc.osgi.bundle.utils.pattern.visitor.IVisitable;
-import org.tc.osgi.bundle.utils.pattern.visitor.IVisitor;
+import org.tc.osgi.bundle.utils.interf.pattern.visitor.IVisitable;
+import org.tc.osgi.bundle.utils.interf.pattern.visitor.IVisitor;
 
 /**
  * Kohonen.java.
+ * 
  * @author collonville thomas
  * @version 0.0.1
  */
 public class Kohonen<T extends Number> extends ReseauNeuronal<T> implements IVisitable {
 
-    /**
-     * long serialVersionUID.
-     */
-    private static final long serialVersionUID = -7564382981250063487L;
-    /**
-     * Neurone<T>[][] matrixVoisinage.
-     */
-    private Neurone<T>[][] matrixVoisinage;
+	/**
+	 * long serialVersionUID.
+	 */
+	private static final long serialVersionUID = -7564382981250063487L;
+	/**
+	 * Neurone<T>[][] matrixVoisinage.
+	 */
+	private Neurone<T>[][] matrixVoisinage;
 
-    /**
-     * Kohonen constructor.
-     * @param nbrNeurone int
-     * @param nbrInput int
-     * @param functionRecur Class
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     */
-    @SuppressWarnings("unchecked")
-    public Kohonen(final int nbrNeurone, final int nbrInput, final Class functionRecur) throws InstantiationException, IllegalAccessException {
-        super();
-        final KohonenFactory<T> factory = new KohonenFactory<T>();
-        final UnidirectionnalLayer<T> uni = factory.createUnidirectionnalLayer(nbrNeurone, nbrInput, Sum.class, functionRecur);
-        getLayers().add(uni);
-        final Iterator<Neurone<T>> itn = uni.getNeurones().iterator();
-        final int dimension = (int) Math.round(Math.sqrt(uni.getNeurones().size()));
-        this.matrixVoisinage = new Neurone[dimension][dimension];
-        int ligne = 0;
-        int colonne = 0;
-        for (; itn.hasNext();) {
-            this.matrixVoisinage[ligne++][colonne] = itn.next();
-            if (ligne == dimension) {
-                ligne = 0;
-                colonne++;
-            }
-        }
-        adaptNeuroneList();
-    }
+	/**
+	 * Kohonen constructor.
+	 * 
+	 * @param nbrNeurone    int
+	 * @param nbrInput      int
+	 * @param functionRecur Class
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	@SuppressWarnings("unchecked")
+	public Kohonen(final int nbrNeurone, final int nbrInput, final Class functionRecur) throws InstantiationException, IllegalAccessException {
+		super();
+		final KohonenFactory<T> factory = new KohonenFactory<T>();
+		final UnidirectionnalLayer<T> uni = factory.createUnidirectionnalLayer(nbrNeurone, nbrInput, Sum.class, functionRecur);
+		getLayers().add(uni);
+		final Iterator<Neurone<T>> itn = uni.getNeurones().iterator();
+		final int dimension = (int) Math.round(Math.sqrt(uni.getNeurones().size()));
+		this.matrixVoisinage = new Neurone[dimension][dimension];
+		int ligne = 0;
+		int colonne = 0;
+		for (; itn.hasNext();) {
+			this.matrixVoisinage[ligne++][colonne] = itn.next();
+			if (ligne == dimension) {
+				ligne = 0;
+				colonne++;
+			}
+		}
+		adaptNeuroneList();
+	}
 
-    /**
-     * @param man AbstractVisitor
-     * @see org.tc.osgi.bundle.utils.pattern.visitor.IVisitable#accept(org.tc.osgi.bundle.utils.pattern.visitor.AbstractVisitor)
-     */
-    @Override
-    public void accept(final IVisitor man) {
-        man.visit(this);
-    }
+	/**
+	 * @param man AbstractVisitor
+	 * @see org.tc.osgi.bundle.utils.pattern.visitor.IVisitable#accept(org.tc.osgi.bundle.utils.pattern.visitor.AbstractVisitor)
+	 */
+	@Override
+	public void accept(final IVisitor man) {
+		man.visit(this);
+	}
 
-    /**
-     * getMatrixVoisinage.
-     * @return Neurone<T>[][]
-     */
-    public Neurone<T>[][] getMatrixVoisinage() {
-        return this.matrixVoisinage;
-    }
+	/**
+	 * getMatrixVoisinage.
+	 * 
+	 * @return Neurone<T>[][]
+	 */
+	public Neurone<T>[][] getMatrixVoisinage() {
+		return this.matrixVoisinage;
+	}
 
-    /**
-     * @return ArrayList<Neurone<T>>
-     * @see org.tc.osgi.bundle.neuronalnetwork.metamodel.mm.Layer#getNeurones()
-     */
-    @Override
-    public ArrayList<Neurone<T>> getNeurones() {
-        return getLayers().get(0).getNeurones();
-    }
+	/**
+	 * @return ArrayList<Neurone<T>>
+	 * @see org.tc.osgi.bundle.neuronalnetwork.metamodel.mm.Layer#getNeurones()
+	 */
+	@Override
+	public ArrayList<Neurone<T>> getNeurones() {
+		return getLayers().get(0).getNeurones();
+	}
 
-    /**
-     * initLayer.
-     * @param dim Dimension
-     */
-    @SuppressWarnings("unchecked")
-    public void initLayer(final Dimension dim) {
-        for (final Neurone<T> n : getLayers().get(0).getNeurones()) {
-            final float valueX = (float) (Math.random() + (dim.width / 2));
-            final float valueY = (float) (Math.random() + (dim.height / 2));
-            if (n.getInputDendrites().size() != 2) {
-                try {
-                    throw new Exception(" la cate de kohonen ne peut etre que de dimension 2 dans ce framework, le vecteur d'entr�e doit etre de dimension 2.");
-                } catch (final Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            final Iterator<Dendrite<T>> itd = n.getInputDendrites().iterator();
-            itd.next().getPoid().setValue((T) new Float(valueX));
-            itd.next().getPoid().setValue((T) new Float(valueY));
-        }
-    }
+	/**
+	 * initLayer.
+	 * 
+	 * @param dim Dimension
+	 */
+	@SuppressWarnings("unchecked")
+	public void initLayer(final Dimension dim) {
+		for (final Neurone<T> n : getLayers().get(0).getNeurones()) {
+			final float valueX = (float) (Math.random() + (dim.width / 2));
+			final float valueY = (float) (Math.random() + (dim.height / 2));
+			if (n.getInputDendrites().size() != 2) {
+				try {
+					throw new Exception(" la cate de kohonen ne peut etre que de dimension 2 dans ce framework, le vecteur d'entr�e doit etre de dimension 2.");
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+			}
+			final Iterator<Dendrite<T>> itd = n.getInputDendrites().iterator();
+			itd.next().getPoid().setValue((T) new Float(valueX));
+			itd.next().getPoid().setValue((T) new Float(valueY));
+		}
+	}
 
-    /**
-     * learn.
-     * @param learningValue ArrayList<Signal<T>>
-     */
-    public void learn(final ArrayList<Signal<T>> learningValue) {
-        getLearningMethod().learn(this, learningValue);
-    }
+	/**
+	 * learn.
+	 * 
+	 * @param learningValue ArrayList<Signal<T>>
+	 */
+	public void learn(final ArrayList<Signal<T>> learningValue) {
+		getLearningMethod().learn(this, learningValue);
+	}
 
-    /**
-     * setMatrixVoisinage.
-     * @param matrixVoisinage Neurone<T>[][]
-     */
-    public void setMatrixVoisinage(final Neurone<T>[][] matrixVoisinage) {
-        this.matrixVoisinage = matrixVoisinage;
-    }
+	/**
+	 * setMatrixVoisinage.
+	 * 
+	 * @param matrixVoisinage Neurone<T>[][]
+	 */
+	public void setMatrixVoisinage(final Neurone<T>[][] matrixVoisinage) {
+		this.matrixVoisinage = matrixVoisinage;
+	}
 
 }
